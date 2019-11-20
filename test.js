c07dfb3042cab2b1ab8442dcde140517854d7a76
Sunbridger <739272884@qq.com>
2019-11-20 14:39:07 +0800

    fix: 恢复记录

--- test.js
+++ test.js
@@ -2,23 +2,22 @@ const puppeteer = require('puppeteer');
 const path = p => require('path').resolve(__dirname, p);
 const autoScroll = require('./autoScroll');
 async function scrape() {
-    const browser = await puppeteer.launch({headless: true });
+    const browser = await puppeteer.launch({headless: false });
     const page = await browser.newPage();
-    await page.goto('https://m.weibo.cn/search?containerid=231583', {
+    await page.goto('https://m.weibo.cn/p/index?containerid=106003type%3D25%26t%3D3%26disable_hot%3D1%26filter_type%3Drealtimehot&title=%E5%BE%AE%E5%8D%9A%E7%83%AD%E6%90%9C', {
         timeout: 3000000
     });
     await page.setViewport({
         width: 1920,
         height: 1080
     });
-    await page.waitFor(2000);
-    // TODO
-    const result = await page.evaluate(() => {
-        const arrs = Array.from(document.querySelectorAll('#app > div:nth-child(1) > div:nth-child(1) > div.card.m-panel.card16.m-col-2 > div > div .m-item-box .m-text-cut')).map(ele => ele.innerText);
-        return arrs;
+    // await autoScroll(page);
+    await page.screenshot({
+        path: path('bb.png'),
+        fullPage: true
     });
     await browser.close();
-    return result;
+
 }
 
 scrape().then(result => {
